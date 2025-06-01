427c2f30560d0c5df083dcf34271d2ed8a01b078
RevellX <kuras.patryk@outlook.com>
2025-06-01 10:23:25 +0200

    Proper-ish... implementation of simple two-way list

--- classes/utils/myList/MyList.cs
+++ classes/utils/myList/MyList.cs
@@ -1,37 +1,62 @@
-using System.Collections;
-
 namespace argumentParser.Classes.Utils;
 
 class MyList<T>
 {
-    private Node<T> first;
-
-    private Node<T> last;
-
-    private int _length = 0;
-
-    public void Add(T obj)
+    private Node<T>? _head;
+    private Node<T>? _tail;
+    private int _count;
+    public MyList()
     {
-        _length++;
-        var current = new Node<T>(obj);
-        if (first == null)
+        _head = null;
+        _tail = null;
+        _count = 0;
+    }
+    public void Add(T item)
+    {
+        Node<T> newNode = new Node<T>(item);
+        if (_head == null)
         {
-            first = current;
-            last = current;
+            _head = newNode;
+            _tail = newNode;
         }
         else
         {
-            var tempLast = last;
-            last.next = current;
-            current.previous = tempLast;
+            _tail!.next = newNode;
+            newNode.previous = _tail;
+            _tail = newNode;
+        }
+        _count++;
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index >= _count)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
         }
     }
 
     public void DisplayItems()
     {
-        for (int i = 0; i < _length; i++)
+        Node<T>? current = _head;
+        while (current != null)
         {
-
+            Logger.DisplayMessage(current.value?.ToString() ?? "");
+            current = current.next;
         }
+    }
+    public int Count()
+    {
+        return _count;
+    }
+    public bool IsEmpty()
+    {
+        return _count == 0;
+    }
+    public void Clear()
+    {
+        _head = null;
+        _tail = null;
+        _count = 0;
     }
 }
